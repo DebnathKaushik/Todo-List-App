107d04aaea0ba0a09c5712c91ebc36e330bee380
DebnathKaushik <debnathkaushik49@gmail.com>
2025-02-26 18:34:52 +0600

    Delete Button Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 
-const Display = ({ list }) => {
-  
+const Display = ({ list,Delete}) => {
   return (
       <div >
       <h2>
       <ol>
           {list.map((item) => (
-            <li key={item.id}>{item}</li>
+            <li key={item.id}>{item.name}
+             <button id="delete_button" onClick={()=>Delete(item.id)}>Delete</button>
+            </li>
           ))}
       </ol>
       </h2>
-    </div>
+    </div> 
   );
 };
 
@@ -28,7 +29,8 @@ const InputBox = ({text,changetext,final})=>{
     <div className='input'>
       <input type='text' 
        placeholder='add item...' 
-       id='input' value={text} 
+       id='input' 
+       value={text} 
        onChange={(e)=>changetext(e.target.value)} 
        onKeyDown={handleKeyDown}/>
 
@@ -41,23 +43,30 @@ const InputBox = ({text,changetext,final})=>{
 
 const App = ()=>{
   const[item,Setitem] = useState("")
-  const[previouslist,Setlist] = useState([])
+  const[updatelist,Setlist] = useState([])
 
   const Addlist = ()=>{
     if(item.trim()){
-      Setlist([...previouslist,item])
+      const newitem = {id:Date.now(), name:item}
+      Setlist([...updatelist,newitem])
       Setitem("")
     }
   }
 
+  const Deletelist = (id)=>{
+    Setlist(updatelist.filter((item)=> item.id !== id))
+  }
+
   return (
     <>
-      <div className='list'>
+    <div className='list'>
       <h1>FABLE</h1>
     </div>
-    <hr/><br/><br/>
+    <hr/>
+    <br/>
+    <br/>
     <InputBox text={item} changetext={Setitem} final={Addlist}/><br/>
-    <Display list={previouslist}/>
+    <Display list={updatelist} Delete={Deletelist}/>
     
     </>
     
